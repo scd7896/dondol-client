7e8c132e18f6306ea058d817549c26c0e8680a8c
scd7896 <scd1212@naver.com>
2021-06-05 15:16:36 +0900

    리퀘스트 객체 만듦

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import "./App.css";
+import request from "./lib/request";
 
 function App() {
+	useEffect(() => {
+		request.get("/post");
+	}, []);
 	return <div className="App">ddsdsds</div>;
 }
 
---
+++ src/lib/request.js
@@ -0,0 +1,57 @@
+import * as qs from "qs";
+import axios from "axios";
+import { getCookie } from "./cookie";
+
+const preRequestSet = (config) => {
+	config.headers.common["Authorization"] = `${getCookie("token")}`;
+	return config;
+};
+
+const preErrorSet = (err) => {
+	return Promise.reject(err);
+};
+
+const responseHandle = (res) => res;
+const errorHandle = (err) => {
+	if (err.response.data.message) {
+		alert(err.response.data.message);
+	}
+	return Promise.reject(err);
+};
+
+class RequestObject {
+	static _instance;
+	client;
+	constructor() {
+		this.client = axios.create({
+			baseURL: "http://localhost:9100",
+		});
+		this.client.interceptors.request.use(preRequestSet, preErrorSet);
+		this.client.interceptors.response.use(responseHandle, errorHandle);
+	}
+	static get Instance() {
+		return this._instance || (this._instance = new this());
+	}
+
+	get(url, query) {
+		return this.client.get(`${url}?${qs.stringify(query)}`);
+	}
+
+	post(url, { body, query }) {
+		return this.client.post(`${url}?${qs.stringify(query)}`, body);
+	}
+
+	delete(url, query) {
+		return this.client.delete(`${url}?${qs.stringify(query)}`);
+	}
+
+	put(url, { body, query }) {
+		return this.client.put(`${url}?${qs.stringify(query)}`, body);
+	}
+
+	patch(url, { body, query }) {
+		return this.client.patch(`${url}?${qs.stringify(query)}`, body);
+	}
+}
+
+export default RequestObject.Instance;
