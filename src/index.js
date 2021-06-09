c0f9266e247e7ff5065dee3b702e3d4219029544
scd7896 <scd1212@naver.com>
2021-06-09 19:14:40 +0900

    ie ployfill 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import "antd/dist/antd.css";
 
 ReactDom.render(<App />, document.getElementById("root"));
